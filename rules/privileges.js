0f2149adbd833e81440f97eab86c2ed5dbe042f7
Raghav Darisi <raghav.darisi@ge.com>
2018-09-18 05:34:21 -0500

    fixed scopes

--- rules/privileges.js
+++ rules/privileges.js
@@ -4,27 +4,30 @@ function (user, context, callback) {
     var clientScopes = context.request.body.scope || context.request.query.scope;
 
     clientScopes = clientScopes.split(" ");
-    clientScopes = _.without(clientScopes, "items:read", "items:write", "items:delete");
+    var clientScopesINC = _.intersection(clientScopes, ["items:read", "items:write", "items:delete"]);
+    var clienScopesTBI = [];
+    //var clientScopesWO = _.without(clientScopes, "items:read", "items:write", "items:delete");
 
     user.app_metadata = user.app_metadata || [];
     user.app_metadata.role = user.app_metadata.role || '';
     
     switch(user.app_metadata.role.toLowerCase()) {
         case 'child': 
-            clientScopes.push("items:read");
+            clienScopesTBI.push("items:read");
             break;
         case 'adult':
-            clientScopes.push("items:read");
-            clientScopes.push("items:write");
+            clienScopesTBI.push("items:read");
+            clienScopesTBI.push("items:write");
             break;
         case 'admin':
-            clientScopes.push("items:read");
-            clientScopes.push("items:write");
-            clientScopes.push("items:delete");
+            clienScopesTBI.push("items:read");
+            clienScopesTBI.push("items:write");
+            clienScopesTBI.push("items:delete");
             break;        
     }
-
-
+    var clientScopesTBI = _.intersection(clientScopesINC, clienScopesTBI);
+    clientScopes = _.without(clientScopes, "items:read", "items:write", "items:delete");
+    clientScopes = _.union(clientScopes, clientScopesTBI);
     context.accessToken.scope = clientScopes.join(" ");
     callback(null, user, context);
   }
