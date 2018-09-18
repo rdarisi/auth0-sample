7edd23b3e1c3b520c39952f7b4b6bf34ebe12ddf
Raghav Darisi <raghav.darisi@ge.com>
2018-09-18 00:25:57 -0500

    ajax calls

--- www/public/js/app.js
+++ www/public/js/app.js
@@ -4,9 +4,35 @@ var deleteItem = function(id) {
         url: "/api/v1/items/" + id,
         type: 'DELETE',
         success: function(resp) {
-            
+            $("#error").html(resp.message).show();
+            document.location.reload();
         },
+        error: function(resp) {
+            $("#error").html(resp.responseJSON.message).show();
+        }
       });
 }
 
+var addItem = function() {
+    $.post("/api/v1/items/", {
+        "name": $("#name").val(),
+        "modelId": $("modelId").val(),
+        "location": $("#location").val(),
+        "connectivity": $("#connectivity").val().split(" "),
+        "controllers": $("#controllers").val().split(" "),
+        "credentials": {
+            "username": $("#username").val(),
+            "password": $("#password").val()
+        },
+        "type": $("#type").val()
 
+    }, function(resp) {
+        if(resp.code == 201) {
+            $("#error").html(resp.message).show();
+            document.location.reload();
+        }
+        else {
+            $("#error").html("Something went wrong!").show();
+        }
+    })
+}
